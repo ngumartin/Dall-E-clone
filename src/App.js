6b8887a297403d134f762186ff2ebafadec4fc6f
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-06-22 17:51:33 -0700

    Start with app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@
 const App = () => {
   return (
     <div className="app">
-      
+      <section className="search-section">
+        <p>Write a detailed description 
+          <span className="generate">Generate</span>
+        </p>
+      </section>
+      <section className="image-section"></section>
     </div>
   )
 }
