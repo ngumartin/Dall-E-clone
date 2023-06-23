495cc727a899da04d778334d0edc8de1e9187cb5
Martin Vuong Nguyen <ngumartin@gmail.com>
2023-06-23 13:56:36 -0700

    Set up continuation.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 const App = () => {
+  const surpriseOptions = [
+    'A dwarf paladin warrior heavily plated in armor, realistic high definition image',
+    'A orc warrior berserker, realistic high definition image',
+    'A human mage, realistic high definition image'
+  ]
+
   return (
     <div className="app">
       <section className="search-section">
         <p>Write a detailed description 
-          <span className="generate">Generate</span>
+          <span className="surprise">Surprise Me</span>
         </p>
+        <div className="input-container">
+          <input 
+            placeholder="A dwarf paladin warrior heavily plated in armor, realistic high definition image..."
+          />            
+          <button>Generate</button> 
+        </div>
       </section>
       <section className="image-section"></section>
     </div>
